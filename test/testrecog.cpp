3d4b8bc5baf7f399e5d25838060f8149fecc3ff9
Andrew <aicpp@yandex.ru>
2019-12-07 12:53:26 +0300

    initial version of recogniser class

--- test/testrecog.cpp
+++ test/testrecog.cpp
@@ -1,6 +1,22 @@
+#include <vector>
+#include <iostream>
 #include "recogniser.h"
+
+using namespace std;
+
+void testOfRecogniserCreation()
+{
+  Recognizer t;
+  vector<vector<int>> data;
+
+  t.load("test.pnm");
+  data = t.recognise();
+
+  cout<< "test testOfRecogniserCreation - OK"<<endl;
+}
 
 int main()
 {
+  testOfRecogniserCreation();
 
 }
